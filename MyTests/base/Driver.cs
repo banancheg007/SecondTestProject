c6c30d9b4a3980af98183d2782ce9b942abaae13
banancheg007 <ruslansolodovnyk@gmail.com>
2019-05-22 10:00:56 +0300

    first commit

---
+++ MyTests/base/Driver.cs
@@ -0,0 +1,38 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.IE;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstTestProject.main
+{
+    static class Driver
+    {
+        private static IWebDriver driver;
+
+        public static IWebDriver Instance()
+        {
+            InternetExplorerOptions options = new InternetExplorerOptions();
+            if (driver == null)
+            {
+                options.EnableNativeEvents = false;
+                driver = new InternetExplorerDriver(options);
+            }
+
+            return driver;
+        }
+
+        static public void Destroy()
+        {
+            if (driver != null)
+            {
+                driver.Close();
+                driver.Quit();
+            }
+        }
+    }
+}
---
+++ MyTests/pages/MainPage.cs
@@ -0,0 +1,13 @@
+﻿using FirstTestProject.page;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyTests.pages
+{
+    class MainPage: Actions
+    {
+    }
+}
